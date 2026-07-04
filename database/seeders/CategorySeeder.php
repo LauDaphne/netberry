dcb430b9c24eb9ec238eb45bf5ff39ca9f416400
DaphneMP <lmunoz@gainkode.com>
2026-07-04 02:04:13 +0200

    ADD - EDIT - Seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        Category::upsert([
+            ['name' => 'PHP'],
+            ['name' => 'Javascript'],
+            ['name' => 'CSS'],
+        ],
+            ['name']
+        );
+    }
+}
